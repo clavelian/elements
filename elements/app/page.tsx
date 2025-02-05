c95702c10cda4824a1fe5a5199beb806bb5a48db
Ian Clavel <ianclavel1991@gmail.com>
2025-02-06 03:30:38 +0800

    minor

--- elements/app/page.tsx
+++ elements/app/page.tsx
@@ -2,7 +2,7 @@ import ResponsiveModals from "@/components/responsiveModal";
 
 export default function Home() {
   return (
-    <div className="grid items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-cols-3 items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
       <ResponsiveModals />
       <ResponsiveModals />
       <ResponsiveModals />
